3cf8c65c825ed8ef1fc62a63233d0c758f66168b
Marco Afonso Albino Ramião <marcoafonso1706@gmail.com>
2026-08-06 21:37:31 +0100

    Now reading from stdin instead of using command-line args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::{env, error::Error, fs, process};
+use std::io::{self, BufRead};
+use std::{/*env,*/ fs, process};
 
 fn main() {
-    let prog = Program::build(env::args()).
-    unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
+    let stdin = io::stdin();
+    let mut path = String::new();
+    if let Err(err) = stdin.lock().read_line(&mut path) {
+        eprintln!("Problem reading from stdin: {err}");
         process::exit(1)
-    });
+    };
+    let path = path.trim().to_string();
+
+    let prog = Program::build(path);
+
+    // let prog = Program::build(env::args()).
+    // unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1)
+    // });
 
     if let Err(err) = run(prog) {
         eprintln!("Application error: {err}");
@@ -13,8 +24,8 @@ fn main() {
     }
 }
 
-fn run(prog: Program) -> Result<(), Box<dyn Error>> {
-    let instrs = fs::read_to_string(prog.path)?;
+fn run(prog: Program) -> Result<(), Box<dyn std::error::Error>> {
+    let instrs = fs::read_to_string(&prog.path)?;
     for instr in instrs.lines() {
         println!("{instr}");
     }
@@ -26,12 +37,15 @@ struct Program {
 }
 
 impl Program {
-    fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
-        args.next();
-        let path = match args.next() {
-            Some(fp) => fp,
-            None => return Err("not enough arguments"),
-        };
-        Ok(Program {path})
+    fn build(path: String) -> Program {
+        Program {path,}
     }
+    // fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+    //     args.next();
+    //     let path = match args.next() {
+    //         Some(fp) => fp,
+    //         None => return Err("not enough arguments"),
+    //     };
+    //     Ok(Program {path})
+    // }
 }
