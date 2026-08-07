cc38c1751ce4a4f24f502a5e7c947af1ee8a2c8e
Marco Afonso Albino Ramião <marcoafonso1706@gmail.com>
2026-08-07 22:52:09 +0100

    piping the contents of the given file, and (for now) writing them to the stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,38 @@
-use std::io::{self, BufRead};
-use std::{/*env,*/ fs, process};
+use std::io::{self, Read,};
+use std::process;
+use std::error::Error;
 
 fn main() {
-    let stdin = io::stdin();
-    let mut path = String::new();
-    if let Err(err) = stdin.lock().read_line(&mut path) {
-        eprintln!("Problem reading from stdin: {err}");
+    let program = Program::build()
+        .unwrap_or_else(|err| {
+            eprintln!("error reading from stdin: {err}");
+            process::exit(1)
+        });
+
+    if let Err(err) = run(&program) {
+        eprintln!("error: {err}");
         process::exit(1)
     };
-    let path = path.trim().to_string();
-
-    let prog = Program::build(path);
-
-    // let prog = Program::build(env::args()).
-    // unwrap_or_else(|err| {
-    //     eprintln!("Problem parsing arguments: {err}");
-    //     process::exit(1)
-    // });
-
-    if let Err(err) = run(prog) {
-        eprintln!("Application error: {err}");
-        process::exit(1)
-    }
 }
 
-fn run(prog: Program) -> Result<(), Box<dyn std::error::Error>> {
-    let instrs = fs::read_to_string(&prog.path)?;
-    for instr in instrs.lines() {
-        println!("{instr}");
-    }
+fn run(prog: &Program) -> Result<(), Box<dyn Error>> {
+    println!("{}", prog.bril);
+
     Ok(())
 }
 
 struct Program {
-    path: String
+    bril: String,
 }
 
 impl Program {
-    fn build(path: String) -> Program {
-        Program {path,}
+    fn build() -> Result<Self, &'static str> {
+        let stdin = io::stdin();
+        
+        let mut bril = String::new();
+        let Ok(_) = stdin.lock().read_to_string(&mut bril) else {
+            return Err("io::stdin()::read_to_string()");
+        };
+        Ok(Program {bril,})
     }
-    // fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
-    //     args.next();
-    //     let path = match args.next() {
-    //         Some(fp) => fp,
-    //         None => return Err("not enough arguments"),
-    //     };
-    //     Ok(Program {path})
-    // }
 }
